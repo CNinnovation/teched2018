9b36db575e9a2f1fedcf368865a515784e22beb8
Christian Nagel <christian@christiannagel.com>
2018-05-14 08:01:26 +0200

    WPF sample 1

---
+++ 01_WPF/DISampleViewModels/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using DISampleViewModels.Services;
+using Prism.Commands;
+
+namespace DISampleViewModels.ViewModels
+{
+    public class MainViewModel
+    {
+        private readonly IShowMessageService _showMessageService;
+        public MainViewModel(IShowMessageService showMessageService)
+        {
+            _showMessageService = showMessageService;
+
+            MessageCommand = new DelegateCommand(OnMessage);
+        }
+
+        public DelegateCommand MessageCommand { get; }
+
+        private async void OnMessage() => await _showMessageService.ShowMessageAsync("Hello from the view-model!");
+    }
+}
---
+++ 01_WPF/WPFDISample/ApplicationServices.cs
@@ -0,0 +1,32 @@
+﻿using DISampleViewModels.Services;
+using DISampleViewModels.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using WPFDISample.Services;
+
+namespace WPFDISample
+{
+    public class ApplicationServices
+    {
+        private ApplicationServices()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<MainViewModel>();
+            services.AddSingleton<IShowMessageService, WPFShowMessageService>();
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        private static ApplicationServices _instance;
+        private static object _instanceLock = new object();
+        private static ApplicationServices GetInstance()
+        {
+            lock (_instanceLock)
+            {
+                return _instance ?? (_instance = new ApplicationServices());
+            }
+        }
+        public static ApplicationServices Instance => _instance ?? GetInstance();
+
+        public IServiceProvider ServiceProvider { get; }
+    }
+}
